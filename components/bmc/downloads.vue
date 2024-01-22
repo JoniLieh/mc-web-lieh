8e029da5a76b78ebf316f3e36716819cd0296d7f
JoniLieh <jonasliehmann@gmail.com>
2024-01-22 16:02:03 +0100

    added modpack

--- components/bmc/downloads.vue
+++ components/bmc/downloads.vue
@@ -7,6 +7,11 @@
         </a>
       </li>
       <li>
+        <a href="https://www.dropbox.com/scl/fo/me0c00s1k7m53hbvm1yje/h?rlkey=4nhi8ybtofrv3qjr6noycpfjo&dl=0" target="_blank">
+          Modpack BetterMC <v-icon color="primary" end>mdi-link-variant</v-icon>
+        </a>
+      </li>
+      <li>
         <a href="https://download.oracle.com/java/17/latest/jdk-17_windows-x64_bin.exe" target="_blank">
           Java 17 JDK
           <v-icon color="primary" end>mdi-link-variant</v-icon>
