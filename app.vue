43d38bfb74cb59abaf2df7cd72859f0c711d6229
JoniLieh <jonasliehmann@gmail.com>
2024-01-22 19:56:07 +0100

    added SEO stuff

--- app.vue
+++ app.vue
@@ -3,7 +3,7 @@
 		<NuxtPwaManifest />
 
 		<NuxtLoadingIndicator />
-		
+
 		<NuxtLayout>
 			<v-sheet class="pa-4 rounded">
 				<NuxtPage />
@@ -13,24 +13,32 @@
 </template>
 
 <script lang="ts" setup>
-	import { usePreferredDark } from '@vueuse/core'
-	import { useTheme } from 'vuetify'
-	const theme = useTheme()
+import { usePreferredDark } from '@vueuse/core'
+import { useTheme } from 'vuetify'
+const theme = useTheme()
 
-	onMounted(() => {
-		theme.global.name.value = usePreferredDark() ? 'dark' : 'light'
-	});
+onMounted(() => {
+	theme.global.name.value = usePreferredDark() ? 'dark' : 'light'
+});
+
+
+useSeoMeta({
+	title: 'Minecraft - JoniLieh',
+	ogTitle: 'My Amazing Site',
+	description: 'Minecraft Installation Tutorials.',
+	ogDescription: 'Minecraft Installation Tutorials.'
+})
 </script>
 
 <style>
 .page-enter-active,
 .page-leave-active {
-  transition: all 0.2s;
+	transition: all 0.2s;
 }
 
 .page-enter-from,
 .page-leave-to {
-  opacity: 0;
-  filter: blur(1rem);
+	opacity: 0;
+	filter: blur(1rem);
 }
 </style>
